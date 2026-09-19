8874ed8f183f5dc0ec1081c6f043a23d545ac699
binadiamarasinghe71-cloud <binadiamarasinghe71@gmail.com>
2026-09-20 00:20:55 +0530

    Test with wildcard CORS origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,12 +12,12 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+app.options('*', cors());
 app.options('*', cors({
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
